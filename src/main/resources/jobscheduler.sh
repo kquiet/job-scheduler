51511c5a7f516948e414e18135b14cc150c10da7
Kimberly <mrkid863@gmail.com>
2019-11-23 16:52:20 +0800

    remove classpath of jar

--- src/main/resources/jobscheduler.sh
+++ src/main/resources/jobscheduler.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java -Dwebdriver.gecko.driver=geckodriver -Dwebdriver.chrome.driver=chromedriver -Dchrome_sandbox=no -jar ${project.build.finalName}.jar
+exec java -Dwebdriver.gecko.driver=geckodriver -Dwebdriver.chrome.driver=chromedriver -Dchrome_sandbox=no -cp "lib/*" org.kquiet.jobscheuler.Launcher
