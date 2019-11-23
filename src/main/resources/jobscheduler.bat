b07cfa3f59675e79c900e4e4c0764224e4c64fdb
Kimberly <mrkid863@gmail.com>
2019-11-23 17:10:28 +0800

    fix type error in scripts

--- src/main/resources/jobscheduler.bat
+++ src/main/resources/jobscheduler.bat
@@ -1,2 +1,2 @@
 cd %~dp0
-start "" javaw -Dwebdriver.gecko.driver=geckodriver.exe -Dwebdriver.chrome.driver=chromedriver.exe -cp "lib/*" org.kquiet.jobscheuler.Launcher
+start "" javaw -Dwebdriver.gecko.driver=geckodriver.exe -Dwebdriver.chrome.driver=chromedriver.exe -cp "lib/*" org.kquiet.jobscheduler.Launcher
--- src/main/resources/jobscheduler.sh
+++ src/main/resources/jobscheduler.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java -Dwebdriver.gecko.driver=geckodriver -Dwebdriver.chrome.driver=chromedriver -Dchrome_sandbox=no -cp "lib/*" org.kquiet.jobscheuler.Launcher
+exec java -Dwebdriver.gecko.driver=geckodriver -Dwebdriver.chrome.driver=chromedriver -Dchrome_sandbox=no -cp "lib/*" org.kquiet.jobscheduler.Launcher
