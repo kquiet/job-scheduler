c72c9af0a3b22e67846f40c80b244db7d1d61bb6
Kimberly <mrkid863@gmail.com>
2019-11-23 20:49:29 +0800

    headless for shell script

--- src/main/resources/jobscheduler.sh
+++ src/main/resources/jobscheduler.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java -Dwebdriver.gecko.driver=geckodriver -Dwebdriver.chrome.driver=chromedriver -Dchrome_sandbox=no -cp "lib/*" org.kquiet.jobscheduler.Launcher
+exec java -Dwebdriver.gecko.driver=geckodriver -Dwebdriver.chrome.driver=chromedriver -Dchrome_sandbox=no -Dwebdriver_headless=yes -cp "lib/*" org.kquiet.jobscheduler.Launcher
