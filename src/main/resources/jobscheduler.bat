a3485d111fec0f6b674e090c4c22050abff4ceb3
Kimberly <mrkid863@gmail.com>
2019-11-23 22:55:31 +0800

    add lib to classpath for scripts

--- src/main/resources/jobscheduler.bat
+++ src/main/resources/jobscheduler.bat
@@ -1,2 +1,2 @@
 cd %~dp0
-start "" javaw -Dwebdriver.gecko.driver=geckodriver.exe -Dwebdriver.chrome.driver=chromedriver.exe -cp "lib/*" org.kquiet.jobscheduler.Launcher
+start "" javaw -Dwebdriver.gecko.driver=geckodriver.exe -Dwebdriver.chrome.driver=chromedriver.exe -cp "lib/;lib/*" org.kquiet.jobscheduler.Launcher
--- src/main/resources/jobscheduler.sh
+++ src/main/resources/jobscheduler.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java -Dwebdriver.gecko.driver=geckodriver -Dwebdriver.chrome.driver=chromedriver -Dchrome_sandbox=no -Dwebdriver_headless=yes -cp "lib/*" org.kquiet.jobscheduler.Launcher
+exec java -Dwebdriver.gecko.driver=geckodriver -Dwebdriver.chrome.driver=chromedriver -Dchrome_sandbox=no -Dwebdriver_headless=yes -cp "lib/:lib/*" org.kquiet.jobscheduler.Launcher
