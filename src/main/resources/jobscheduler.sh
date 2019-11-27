0fefac15953209c05fab10c60f1f79f670e955b4
Kimberly <mrkid863@gmail.com>
2019-11-27 09:19:31 +0800

    add directory 'ext' to classpath

--- src/main/resources/jobscheduler.sh
+++ src/main/resources/jobscheduler.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java -Dwebdriver.gecko.driver=geckodriver -Dwebdriver.chrome.driver=chromedriver -Dchrome_sandbox=no -Dwebdriver_headless=yes -cp "lib/:lib/*" org.kquiet.jobscheduler.Launcher
+exec java -Dwebdriver.gecko.driver=geckodriver -Dwebdriver.chrome.driver=chromedriver -Dchrome_sandbox=no -Dwebdriver_headless=yes -cp "lib/:lib/*:ext/:ext/*" org.kquiet.jobscheduler.Launcher
